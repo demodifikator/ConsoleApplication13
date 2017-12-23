a66242adc56c7791093a03867ce3a41ec9cf9fb2
student <student@D40>
2017-12-23 15:19:14 +0200

    gm

--- ConsoleApplication13/Program.cs
+++ ConsoleApplication13/Program.cs
@@ -6,10 +6,68 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication13
 {
+    public class Bake
+    {
+        private int Temperature;
+
+        public int TemperatureProp
+        {
+            get
+            {
+                return Temperature;
+            }
+
+            set
+            {
+                if (value > 200 || value < 0)
+                    Console.WriteLine("It's wrong!");
+                else Temperature = value;
+            }
+        }
+    }
+    public virtual void OnBake()
+    {
+
+    }
+
+    public class ElectricBake: Bake
+    {
+        public override void OnBake();
+        public void OnBake()
+        {
+            Console.WriteLine("Electric bake is heating!");
+        }
+    }
+    public class GasBake: Bake
+    {
+        public void OnBake()
+        {
+            Console.WriteLine("Gas bake is heating!");
+        }
+    }
+    class Ivan
+    {
+        public string Name{ get; set; }
+
+        public void OnBake(Bake bake)
+        {
+            bake.OnBake();
+        }
+       
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Bake bake = new Bake();
+            bake.TemperatureProp = 1;
+            Console.WriteLine(bake.TemperatureProp);
+            ElectricBake electricBake = new ElectricBake();
+            electricBake.TemperatureProp = 16;
+
+            Ivan ivan = new Ivan();
+            ivan.OnBake;
         }
     }
 }
